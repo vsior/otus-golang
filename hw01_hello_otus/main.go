59753cb6058c419b4e9b3e4ab9a8cff4265af7a1
Ilya.Aleshin <Ilya.Aleshin@x5.ru>
2025-05-31 13:19:41 +0300

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,15 @@
 package main
 
+import "fmt"
+
+func reverse(s string) string {
+	r := make([]byte, len(s))
+	for i := range len(s) {
+		r[i] = s[len(s)-1-i]
+	}
+	return string(r)
+}
+
 func main() {
-	// Place your code here.
+	fmt.Println(reverse("Hello, OTUS!"))
 }
