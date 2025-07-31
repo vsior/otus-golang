0dbd1efe70117932fada46853edc5f001fea3332
Ilya.Aleshin <Ilya.Aleshin@x5.ru>
2025-07-31 13:14:28 +0300

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,78 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
 )
 
 var (
+	ErrOpenFile              = errors.New("open file error")
+	ErrIncorrectOffset       = errors.New("incorrect offset")
 	ErrUnsupportedFile       = errors.New("unsupported file")
+	ErrCopying               = errors.New("failed to copy file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
+const maxCopyLen = int64(1024)
+
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	if fromPath == toPath {
+		return ErrCopying
+	}
+	fromFile, err := os.Open(fromPath)
+	if err != nil {
+		return ErrOpenFile
+	}
+	defer fromFile.Close()
+
+	stat, err := fromFile.Stat()
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+
+	if offset > stat.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if offset < 0 {
+		offset = 0
+	}
+
+	copySize := stat.Size() - offset
+	if limit > 0 && limit < copySize {
+		copySize = limit
+	}
+
+	_, err = fromFile.Seek(offset, io.SeekStart)
+	if err != nil {
+		return ErrIncorrectOffset
+	}
+
+	toFile, err := os.Create(toPath)
+	if err != nil {
+		return ErrOpenFile
+	}
+	defer toFile.Close()
+
+	progressChan := make(chan int64)
+	terminatedPb := progressBar{}.start(progressChan, copySize)
+	defer func() {
+		close(progressChan)
+		<-terminatedPb
+	}()
+
+	copiedBytes := int64(0)
+	copyLen := maxCopyLen
+	for copiedBytes < copySize {
+		if copyLen+copiedBytes > copySize {
+			copyLen = copySize - copiedBytes
+		}
+		_, err := io.CopyN(toFile, fromFile, copyLen)
+		copiedBytes += copyLen
+		progressChan <- copiedBytes
+		if err != nil && !errors.Is(err, io.EOF) {
+			return ErrCopying
+		}
+	}
 	return nil
 }
